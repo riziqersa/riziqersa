8889612399a7efb61c47764e6752b216f90b3354
riziqersa <61577785+riziqersa@users.noreply.github.com>
2020-03-09 00:03:46 +0700

    Create cek.js

---
+++ cek.js
@@ -0,0 +1,35 @@
+—————————————————— [INFORMATION] ————————————————————
+
+  [?] {bold.green Check Username Account *AUTO!}
+
+  [?] {bold.green Save Result to Log.txt}
+
+  ——————————————————  [THANKS TO AEXINAV]  ————————————————————
+
+  [✓] CODER BY AEXINAV (https://fb.me/aexinav)
+
+  [✓] FIXING & TESTING BY AEXINAV
+
+  ——————————————————  [THANKS TO ALFATON]  ————————————————————
+
+  [✓] CODER BY RZQRSDF (https://fb.me/riziq e)
+
+  [✓] FIXING & TESTING BY RIZIIQ.E
+
+  [✓] TOOLS AMAN 1000000% NO LOGGER
+
+  [✓] GROW UP YOUR INSTAGRAM FOLLOWERS NOW!
+
+  —————————————————————————————————————————————————————}
+
+      `);
+
+//aexinav
+
+//alfato.
+
+inquirer.prompt(User)
+
+.then(answers => {
+
+  Excute({
