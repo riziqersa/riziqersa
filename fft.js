079b71c7c86cefa56a59eacf3b5ee93694b0bba3
riziqersa <61577785+riziqersa@users.noreply.github.com>
2020-03-09 00:27:16 +0700

    Create fft.js

---
+++ fft.js
@@ -0,0 +1,41 @@
+—————————————————— [INFORMATION] ————————————————————
+
+  [?] {bold.green FFTauto | Follow & Comment Only!}
+
+  [?] {bold.green Gunakan komen.txt untk komen!}
+
+  ——————————————————  [THANKS TO AEXINAV]  ————————————————————
+
+  [✓] CODER BY AEXINAV(https://fb.me/aexinav)
+
+  [✓] FIXING & TESTING BY AEXINAV
+
+  ——————————————————  [THANKS TO RIZIQ]  ————————————————————
+
+  [✓] CODER BY RZQRSDF(https://ig.me/rzqrsdf)
+
+  [✓] FIXING & TESTING BY RZQRSDF
+
+  [✓] TOOLS AMAN 1000000% NO LOGGER
+
+  [✓] GROW UP YOUR INSTAGRAM FOLLOWERS NOW!
+
+  —————————————————————————————————————————————————————
+
+  What's new?
+
+  1. Input Target/delay Manual (ITTYW)
+
+  —————————————————————————————————————————————————————}
+
+      `);
+
+//aexinav
+
+//rzqrsdf
+
+inquirer.prompt(User)
+
+.then(answers => {
+
+  Excute({
