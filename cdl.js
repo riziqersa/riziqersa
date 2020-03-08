a36cc53f06399c3e4c531d1f2b1a36ae96b465b4
riziqersa <61577785+riziqersa@users.noreply.github.com>
2020-03-08 23:47:30 +0700

    Create cdl.js

---
+++ cdl.js
@@ -0,0 +1,41 @@
+[?] {bold.green Comment, DM & Like}
+
+  [?] {bold.green Gunakan komen.txt untk komen!}
+
+  
+
+  ————————————————— [TERIMA KASIH KE AEXINAV ] ——————————————————————
+
+  [✓] CODER BY AEXINAV (https://fb.me/ aexinav )
+
+  [✓] MEMPERBAIKI & MENGUJI OLEH AEXINAV
+
+  ————————————————— [TERIMA KASIH kepada ALFATON ] ——————————————————————
+
+  [✓] CODER BY RZQRSDF (https://fb.me/ Riziq E )
+
+  [✓] MEMPERBAIKI & MENGUJI OLEH RIZIQ
+
+  [✓] ALAT AMAN 1000000% TANPA LOGGER
+
+  [✓] TINGKATKAN PENGIKUT INSTAGRAM ANDA SEKARANG!
+
+  ———————————————————————————————————————————————————— ———
+
+  Apa yang baru?
+
+  1. Masukkan Target / Manual penundaan (ITTYW)
+
+  ————————————————————————————————————————————————————— ———}
+
+      ` ) ;
+
+// aexinav
+
+// rzqrsdf
+
+penanya . prompt ( Pengguna )
+
+. lalu ( jawaban  =>  {
+
+  Excute ( {
